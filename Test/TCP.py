8046a5f5ba87c47b042f4f86ac178837bdebcb54
黃智彬 <44690800+Ziller0831@users.noreply.github.com>
2024-07-23 10:18:19 +0900

    Delete Test directory

--- Test/TCP.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-def Client(host, port, message):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((host, port))
-        s.sendall(message.encode('utf-8'))
-        data = s.recv(1024)
-    return data
